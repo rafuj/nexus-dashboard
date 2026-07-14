fddabf782d437f560af8db3ee9cbc1ddb3736b87
rafuj <raihanrafuj9978@gmail.com>
2026-07-14 10:53:56 +0600

    feat(new-page) generate-serial-number page design)

--- src/app/views/AppRoutes.tsx
+++ src/app/views/AppRoutes.tsx
@@ -21,6 +21,7 @@ import ExploreUpgrades from "@/features/explore-upgrades/views/ExploreUpgrades";
 import MyAccount from "@/features/my-account/views/MyAccount";
 import { useAuth } from "../hooks/useAuth";
 import ImeiLinking from "@/features/factory/views/ImeiLinking";
+import GenerateSerialNumber from "@/features/factory/views/GenerateSerialNumber";
 
 const helmetContext = {};
 
@@ -31,9 +32,14 @@ export default function AppRoutes() {
   const dashboardViewContent = () => {
     switch(user?.role) {
       case "factory":
-        return <ImeiLinking />
+        return <>
+          <Route path="/" element={<ImeiLinking />} />
+          <Route path="/generate-serial-number" element={<GenerateSerialNumber />} />
+        </>
       default :
-        return <DashboardView />
+        return <>
+          <Route path="/" element={<DashboardView />} />
+        </>
     }
   }
 
@@ -54,8 +60,8 @@ export default function AppRoutes() {
             </Route>
 
             <Route element={<PageLayout />}>
-              {/* <Route path="/" element={<DashboardView />} /> */}
-              <Route path="/" element={dashboardViewContent()} />
+
+              {/* Common Routes */}
               <Route path="/cabinets" element={<Navigate to="/cabinets/list" replace />} />
               <Route path="/cabinets/list" element={<CabinetsListView />} />
               <Route path="/cabinets/add" element={<AddCabinets />} />
@@ -66,6 +72,10 @@ export default function AppRoutes() {
               <Route path="/cabinets/list/:id" element={<CabinetView />} />
               <Route path="/settings" element={<Settings />} />
               <Route path="/my-account" element={<MyAccount />} />
+              
+              {/* Routes by Role */}
+              {dashboardViewContent()}
+
             </Route>
             <Route path="/explore-upgrade" element={<ExploreUpgrades />} />
           </Routes>
