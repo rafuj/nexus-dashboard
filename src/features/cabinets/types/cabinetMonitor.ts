a255182f59baaf567d1126d15578eb53754a5d33
rafuj <raihanrafuj9978@gmail.com>
2026-07-30 15:35:10 +0600

    Fix Activity and Cabinet Monitor

--- src/features/cabinets/types/cabinetMonitor.ts
+++ src/features/cabinets/types/cabinetMonitor.ts
@@ -1,18 +1,18 @@
-export type CabinetMonitorToolbarProps = {
-  search: string
-  onSearchChange: (value: string) => void
-  city: string
-  setCity: (value:string)=> void
-  status: string
-  setStatus: (value:string)=> void
-}
-
 
 export type AssetHealth = 'Ok' | 'Warning' | 'Urgent' | 'Paused';
 export type AssetPresence = 'Present' | 'Missing';
 export type DoorStatus = 'Closed' | 'Open';
 export type Status = 'urgent' | 'ok' | 'warning' | 'paused';
 export type FilterStatus = Status | 'all';
+
+export type CabinetMonitorToolbarProps = {
+  search: string;
+  onSearchChange: (value: string) => void;
+  city: string;
+  setCity: (value: string) => void;
+  status: FilterStatus; // Changed from string
+  setStatus: (value: FilterStatus) => void; // Changed from string
+};
 
 export interface CabinetMonitorProps {
   id: string;
