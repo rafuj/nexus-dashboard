9bcdfa7804cb2f530012be67cc3fee1a5f3cfb96
rafuj <raihanrafuj9978@gmail.com>
2026-08-06 17:04:42 +0600

    fix icon color

--- src/features/dashboard/views/DashboardView.tsx
+++ src/features/dashboard/views/DashboardView.tsx
@@ -50,7 +50,7 @@ export default function DashboardView() {
             >
               <div className="col-span-2 py-4 px-5 relative border rounded-[20px] bg-white">
                 <div className="text-sm text-accent-foreground">Network Readiness</div>
-                {<Icons.network className="absolute top-5 right-5" />}
+                {<Icons.network className="absolute top-5 right-5 text-[#A82820]" />}
                 <div>
                   <h3 className="text-2xl font-semibold mt-6 mb-2">
                     96% <span className="text-sm font-normal text-accent-foreground text-primary">ready</span>
